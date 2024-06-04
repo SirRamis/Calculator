49ad5568c8d4a49c7670c10b2235e121fb5c5f5f
Ramis <sir.ramis@mail.ru>
2024-06-04 14:29:46 +0300

    added return

--- calculator.py
+++ calculator.py
@@ -6,14 +6,14 @@ def calculator():
         match = re.match(r"^(\d+)([\+\-\*/])(\d+)$", expression)
         if not match:
             if re.search(r"[\+\-\*/]", expression):
-                raise ValueError("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+                return ValueError("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
             else:
-                raise ValueError("т.к. строка не является математической операцией")
+                return ValueError("т.к. строка не является математической операцией")
 
         a, operator, b = match.groups()
         a, b = int(a), int(b)
         if not (1 <= a <= 10 and 1 <= b <= 10):
-            raise ValueError("Числа должны быть от 1 до 10 включительно")
+            return ValueError("Числа должны быть от 1 до 10 включительно")
         if operator == '+':
             result = a + b
         elif operator == '-':
@@ -22,12 +22,12 @@ def calculator():
             result = a * b
         elif operator == '/':
             if b == 0:
-                raise ValueError("Деление на ноль невозможно")
+                return ValueError("Деление на ноль невозможно")
             result = a // b
         else:
-            raise ValueError("Неподдерживаемая операция")
-        print(result)
+            return ValueError("Неподдерживаемая операция")
+        return result
 
     except Exception as e:
-        print(f"throws Exception: {e}")
+        return f"throws Exception: {e}"
 
