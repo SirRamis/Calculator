759ac41ecae29d28caf1a6bdee9f31b0418daae1
Ramis <sir.ramis@mail.ru>
2024-06-04 15:06:14 +0300

    added return

--- calculator.py
+++ calculator.py
@@ -3,17 +3,24 @@ import re
 def calculator():
     try:
         expression = input()
+        #match = re.match(r"^\s*(\d+)\s*([\+\-\*/])\s*(\d+)\s*$", expression)
         match = re.match(r"^(\d+)([\+\-\*/])(\d+)$", expression)
         if not match:
             if re.search(r"[\+\-\*/]", expression):
-                return ValueError("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+                result = "throws Exception: т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)"
             else:
-                return ValueError("т.к. строка не является математической операцией")
+                result = "throws Exception: т.к. строка не является математической операцией"
+            print(result)
+            return result
 
         a, operator, b = match.groups()
         a, b = int(a), int(b)
+
         if not (1 <= a <= 10 and 1 <= b <= 10):
-            return ValueError("Числа должны быть от 1 до 10 включительно")
+            result = "throws Exception: Числа должны быть от 1 до 10 включительно"
+            print(result)
+            return result
+
         if operator == '+':
             result = a + b
         elif operator == '-':
@@ -22,12 +29,20 @@ def calculator():
             result = a * b
         elif operator == '/':
             if b == 0:
-                return ValueError("Деление на ноль невозможно")
+                result = "throws Exception: Деление на ноль невозможно"
+                print(result)
+                return result
             result = a // b
         else:
-            return ValueError("Неподдерживаемая операция")
+            result = "throws Exception: Неподдерживаемая операция"
+            print(result)
+            return result
+
+        print(result)
         return result
 
     except Exception as e:
-        return f"throws Exception: {e}"
+        result = f"throws Exception: {e}"
+        print(result)
+        return result
 
