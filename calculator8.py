81984b9f580bf3852c6b98079dfee3f52893ea5b
Ramis <sir.ramis@mail.ru>
2024-06-29 08:23:49 +0300

    added __main__

--- calculator8.py
+++ calculator8.py
@@ -15,7 +15,7 @@ def calculator(example):
         parts = example.split(operator)
 
         if len(parts) != 2:
-            raise ValueError("throws Exception//Не является математической операцией")
+            raise ValueError("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
 
         a = parts[0].strip()
         b = parts[1].strip()
