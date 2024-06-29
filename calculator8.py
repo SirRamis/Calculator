74c4992f074d7f364c5bc8675ba5bf5f445817f7
Ramis <sir.ramis@mail.ru>
2024-06-29 08:20:54 +0300

    added __main__

--- calculator8.py
+++ calculator8.py
@@ -1,21 +1,21 @@
-def calculator():
+def calculator(example):
     try:
-        input_string = input().strip()
+        #input_string = input().strip()
         operators = ['+', '-', '*', '/']
         operator_found = False
 
         for operator in operators:
-            if operator in input_string:
+            if operator in example:
                 operator_found = True
                 break
 
         if not operator_found:
             raise ValueError("Неправильный формат ввода")
 
-        parts = input_string.split(operator)
+        parts = example.split(operator)
 
         if len(parts) != 2:
-            raise ValueError("Неправильный формат ввода11")
+            raise ValueError("throws Exception//Не является математической операцией")
 
         a = parts[0].strip()
         b = parts[1].strip()
@@ -43,5 +43,17 @@ def calculator():
         return "Ошибка: " + str(e)
 
 
-result = calculator()
-print(result)
+# result = calculator()
+# print(result)
+
+def main(input_str):
+    try:
+        result = calculator(input_str)
+        return str(result)
+    except ValueError as e:
+        return str(e)
+
+
+if __name__ == "__main__":
+    example = input("Введите пример: ")
+    print(main(example))
